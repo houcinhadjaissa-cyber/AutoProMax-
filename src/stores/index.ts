0b2dc9f03e07b3b5ac5a1547104d77d1a31cec4a
houcinhadjaissa-cyber <houcinhadjaissa@gmail.com>
2026-05-18 20:09:01 +0100

    Add index.ts to export store hooks

---
+++ src/stores/index.ts
@@ -0,0 +1,5 @@
+export { useAuthStore } from './authStore'
+export { useCartStore } from './cartStore'
+export { useSettingsStore } from './settingsStore'
+export { useToastStore } from './toastStore'
+export { useRoleStore } from './roleStore'
